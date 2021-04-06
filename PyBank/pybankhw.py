e17b1c11058b6de286c5a80c116728ad646bfbd7
David W. Mueller <davemuelle@gmail.com>
2021-04-06 09:53:20 -0400

    Add PyBank and PyPoll folders

---
+++ PyBank/pybankhw.py
@@ -0,0 +1,74 @@
+import os
+import csv
+
+#Import path
+dir_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(dir_path)
+#print(dir_path)
+pybank_path = os.path.join(dir_path, 'Resources')
+os.chdir(pybank_path)
+#print(pybank_path)
+
+#Export path
+output_path=os.path.dirname("output_file_pybank.txt")
+#print(output_path)
+
+#Variables
+count=0
+total_profit=0
+pl_list1=[]
+pl_list2=[0]
+date=[]
+change_pl=[]
+
+#Read file
+with open('budget_data.csv','r') as csv_file:
+    csvreader = csv.reader(csv_file, delimiter=',')
+    csv_header=next(csvreader)
+    
+#Lists
+    for row in csvreader:
+        count+=1
+        profit_loss=int(row[1])
+        total_profit+=profit_loss
+        pl_list1.append(profit_loss)
+        pl_list2.append(profit_loss)
+        date.append(row[0])
+    change_pl=[(pl_list1[index] - pl_list2[index]) for index in range(len(pl_list1))]
+
+#Calculations
+    change_pl.pop(0)
+    average_change=sum(change_pl)/len(change_pl)
+    average_change=round(average_change,2)
+    increase= max(change_pl)
+    decrease= min(change_pl)
+    change_pl.insert(0,"N/A")
+
+#Zip file
+    zip_list=zip(date,pl_list1,change_pl)
+    for i in zip_list:
+        if i[2] == increase:
+            increase_date=i[0]
+        if i[2] == decrease:
+            decrease_date=i[0]
+
+#Display
+print("Financial Report")
+print("----------------------------")
+print(f"Total Months: {count}")
+print(f"Net Total in Profits/Loss: ${total_profit}")
+print(f"Average Change in Profits/Loss: ${average_change}")
+print(f"Greatest Increase in Profits/Loss: {increase_date} (${increase})")
+print(f"Greatest Decrease in Profits/Loss: {decrease_date} (${decrease})")
+
+#Write file
+with open("output_file_pybank.txt","w") as datafile:
+    csvwriter = csv.writer(datafile, lineterminator='\n')
+    
+    csvwriter.writerow(["Financial Report"])
+    csvwriter.writerow(["----------------------------"])
+    csvwriter.writerow([f"Total Months: {count}"])
+    csvwriter.writerow([f"Net Total in Profits/Loss: ${total_profit}"])
+    csvwriter.writerow([f"Average Change in Profits/Loss: ${average_change}"])
+    csvwriter.writerow([f"Greatest Increase in Profits/Loss: {increase_date} (${increase})"])
+    csvwriter.writerow([f"Greatest Decrease in Profits/Loss: {decrease_date} (${decrease})"])
---
+++ PyPoll/pypollhw.py
@@ -0,0 +1,95 @@
+import os
+import csv
+
+#Import path
+dir_path = os.path.dirname(os.path.abspath(__file__))
+os.chdir(dir_path)
+#print(dir_path)
+pybank_path = os.path.join(dir_path, 'Resources')
+os.chdir(pybank_path)
+#print(pybank_path)
+
+#Export path
+output_path=os.path.dirname("output_file_pypoll.txt")
+#print(output_path)
+
+#Variables
+vote_total=0
+voterid_list=[]
+county_list=[]
+choice_list=[]
+candidate_list=[]
+khan_vote=0
+correy_vote=0
+li_vote=0
+otooley_vote=0
+
+#Read file
+with open('election_data.csv','r') as csv_file:
+    csvreader = csv.reader(csv_file, delimiter=',')
+    csv_header=next(csvreader)
+
+#Lists
+    for row in csvreader:
+        vote_total+=1
+        voterid=int(row[0])
+        voterid_list.append(voterid)
+        county=row[1]
+        county_list.append(county)
+        candidate=row[2]
+        choice_list.append(candidate)
+    #candidate_list=[(c) for c in choice_list if c not in candidate_list]
+    for c in choice_list:
+        if c not in candidate_list:
+            candidate_list.append(c)
+    #print(candidate_list)
+            
+#Vote totals
+    for v in choice_list:
+        if v == candidate_list[0]:
+            khan_vote+=1
+        if v == candidate_list[1]:
+            correy_vote+=1
+        if v == candidate_list[2]:
+            li_vote+=1
+        if v == candidate_list[3]:
+            otooley_vote+=1
+
+#Percent vote
+    khan=khan_vote/vote_total
+    perc_khan="{:.0%}".format(khan)
+    correy=correy_vote/vote_total
+    perc_correy="{:.0%}".format(correy)
+    li=li_vote/vote_total
+    perc_li="{:.0%}".format(li)
+    otooley=otooley_vote/vote_total
+    perc_otooley="{:.0%}".format(otooley)
+
+#Display
+print('Election Results')
+print('-------------------------')
+print(f'Total Votes: {vote_total}')
+print('-------------------------')
+print(f'Khan: {perc_khan} ({khan_vote})')
+print(f'Correy: {perc_correy} ({correy_vote})')
+print(f'Li: {perc_li} ({li_vote})')
+print(f"O'Tooley: {perc_otooley} ({otooley_vote})")
+print('-------------------------')
+print('Winner: Khan')
+print('-------------------------')
+
+#Write file
+with open("output_file_pypoll.txt","w") as datafile:
+    csvwriter = csv.writer(datafile, lineterminator='\n')
+
+    csvwriter.writerow(['Election Results'])
+    csvwriter.writerow(['-------------------------'])
+    csvwriter.writerow([f'Total Votes: {vote_total}'])
+    csvwriter.writerow(['-------------------------'])
+    csvwriter.writerow([f'Khan: {perc_khan} ({khan_vote})'])
+    csvwriter.writerow([f'Correy: {perc_correy} ({correy_vote})'])
+    csvwriter.writerow([f'Li: {perc_li} ({li_vote})'])
+    csvwriter.writerow([f"O'Tooley: {perc_otooley} ({otooley_vote})"])
+    csvwriter.writerow(['-------------------------'])
+    csvwriter.writerow(['Winner: Khan'])
+    csvwriter.writerow(['-------------------------'])
